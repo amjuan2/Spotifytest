a820dc2c4686a716d21a2d87ca8e5d0ee3c26466
juan <ayalachuqui@gmail.com>
2021-07-24 19:15:15 -0500

    arreglo en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  client_id: "7b6c73e16e4f4315ae6d307213d1588b",
+  client_secret: "4f20f183e9a14af191713f07575a9616",
+  redirect_uri: "http://localhost:4200/home",
+  basic_url: "https://api.spotify.com/v1/",
+  token:""
 };
