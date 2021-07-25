16db5b1ea211cc5c78e9e0be46d652bb666897bf
juan <ayalachuqui@gmail.com>
2021-07-24 19:27:18 -0500

    cambio dominio prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   client_id: "7b6c73e16e4f4315ae6d307213d1588b",
   client_secret: "4f20f183e9a14af191713f07575a9616",
-  redirect_uri: "http://localhost:4200/home",
+  redirect_uri: "testsspotityjuanjose.000webhostapp.com",
   basic_url: "https://api.spotify.com/v1/",
   token:""
 };
